6f29b99ba9ddbdf79c439a6d23f4a05fb1141cd5
danieldamian09 <humbertorivero9@gmail.com>
2022-04-28 16:20:11 -0300

    styles component Navbar

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,8 +1,9 @@
 import Link from "next/link";
+import styles from "./Navbar.module.css";
 
 const Navbar = () => {
   return (
-    <nav>
+    <nav className={styles['menu-container']}>
       <Link href="/">Home</Link>
       <Link href="/about">About</Link>
       <Link href="/contact">Contact</Link>
